ed4199f927e078ab9d13ab6676d74289af264048
vishnukumarkvs <kvsvishnukumar@gmail.com>
2024-11-09 16:03:14 +0530

    completed work and hobbies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,16 +50,16 @@
 			<div class="grid grid-cols-2">
 				<div>
 					<p class="text-7xl font-bold">Hi, I am Vishnu...</p>
-					<p class="py-4 text-xl">
+					<p class="py-4 pt-8 text-2xl font-semibold" style="font-family: 'Comic Neue'">
 						I am a Software Engineer with expertise in Cloud, SRE and Full Stack Dev.
 					</p>
-					<p class="py-4 text-lg">
+					<p class="py-4 text-lg leading-loose" style="font-family: 'Comic Neue', font-weight: 600">
 						I'm striving to become a polyglot, passionate about working with various technologies.
 						I'm a Go and Kubernetes enthusiast who enjoys full-stack development for fun and
 						documents my knowledge on Medium.
 					</p>
 					<div class="my-4">
-						<p class="font-md py-2 text-xl">Here are my socials</p>
+						<p class="py-2 text-xl">Here are my socials</p>
 						<div class="flex">
 							{#each links as link}
 								{@render iconLink(link.icon, link.href)}
@@ -75,8 +75,60 @@
 				<div></div>
 			</div>
 		</Tabs.Content>
-		<Tabs.Content value="work">
-			<p>Thjustify-endis is about my work</p>
+		<Tabs.Content value="work" class="my-5 p-5">
+			<div class="grid grid-cols-2">
+				<div class="flex flex-col space-y-8">
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						I’m a <span class="text-xl font-bold text-sky-500">Software Engineer II at F5</span>,
+						where I balance between Site Reliability Engineering (SRE) and Development. I work with
+						Kubernetes and AWS Cloud, using tools like Helm, Argo, and the LMA stack (Prometheus,
+						Cortex, Loki, and Elasticsearch). I have built detailed Grafana dashboards as well with
+						Grafonnet to give insights at a glance.
+					</p>
+
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						I also have a lot of experience with Infrastructure as Code from my previous org,
+						primarily with Terraform and AWS CDK. I also work with Go, I’ve created a range of
+						custom CLI tools that help make our workflows easier and more efficient.
+					</p>
+
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						In my free time, I enjoy building full-stack projects. I use Svelte for front-end work
+						and either Go or Node.js for the backend. I’ve created a few websites, including
+						{@render Link("https://candletower.com","candletower.com")} and {@render Link("https://www.producthunt.com/products/quickrefer","quickrefer.in")}
+					</p>
+
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						I am a part of {@render Link("https://phinite.ai","Phinite.Ai")}, an AI-driven platform that enhances call center
+						operations. There, I led the DevOps pipeline, using some of the latest technologies to
+						help improve customer interactions and drive operational efficiency.
+					</p>
+				</div>
+				<div></div>
+			</div>
+		</Tabs.Content>
+		<Tabs.Content value="hobbies" class="my-5 p-5">
+			<div class="grid grid-cols-2">
+				<div class="flex flex-col space-y-8">
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						In my free time, you’ll often find me with a book in hand. I’m a huge fan of novels like
+						Norwegian Wood and Before the Coffee Gets Cold. When I’m not reading, I enjoy a good
+						game of chess, or really any game that freshens my mind.
+					</p>
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						I also enjoy building things, whether it’s a side project or experimenting with new
+						tech. Writing about my experiences and what I’ve learned on Medium is something I really
+						enjoy. It’s a way for me to document and share my journey in tech.
+					</p>
+					<p class="p-2 text-xl leading-loose" style="font-family: 'Noto Sans'">
+						I play badminton from time to time to stay active, but I also like to relax with a good
+						movie or anime. One Piece is my all time favourite. I also enjoy the wild humor of Rick
+						and Morty. Whether it's a Netflix series or a great anime, I'm always up for something
+						new.
+					</p>
+				</div>
+				<div></div>
+			</div>
 		</Tabs.Content>
 	</Tabs.Root>
 </div>
@@ -88,3 +140,11 @@
 		</div>
 	</a>
 {/snippet}
+
+{#snippet Link(href, text)}
+	<a {href} class="text-sky-600" target="_blank" rel="noopener noreferer">{text}</a>
+{/snippet}
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Gaegu&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&display=swap');
+</style>
